2baeedf08c6a6a7d0dc3b1846a13b567fbe4199a
kevinj93 <keviny.19933@gmail.com>
2019-01-18 17:36:31 +0100

    username fix + database connection improved
    Apparently username printing wasn't coming from database (fixed now)

--- WEBSERVER/root/process.php
+++ WEBSERVER/root/process.php
@@ -1,15 +1,15 @@
 
 <?php
 
-#initialize the variables for database connection.
+#initialize the variables necessary for database connection.
 $host = "localhost";
-$username = "";
-$password = "";
+$username_db = "root";
+$password_db = "usbw";
 $db = "test";
 $table = "users";
 
 #make a connection / stop if connection failed.
-mysql_connect("$host", "$username", "$password") or die ("connection failed");
+mysql_connect("$host", "$username_db", "$password_db") or die ("connection failed");
 mysql_select_db("$db") or die ("database selection failed");
 
 #Store form data 
@@ -17,27 +17,34 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 #Avoid MySQL injection
-$username = stripslashes($username);
-$password = stripslashes($password);
+$username = mysql_real_escape_string(stripslashes($username));
+$password = mysql_real_escape_string(stripslashes($password));
 
-
-$username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
 
 $query = "SELECT * FROM $table WHERE username='$username' and pass= '$password'";
-$result = mysql_query($query);
+$count = mysql_num_rows(mysql_query($query));
 
 
-$count = mysql_num_rows($result);
+
+
+
 
 #if count > 1 then user exists.
 if ($count == 1)
 	
 	{
+	$query_login = mysql_query("SELECT * FROM $table WHERE username='$username' and pass= '$password'");
+	
+	
 		
 		session_start();
+		
 		$_SESSION['logged-in'] = true;
-		$_SESSION['username'] = $username;
+
+		while ($row = mysql_fetch_assoc($query_login)) {
+		$_SESSION['username'] = $row['username'];}
+
+
 		header("Location: index.php");
 		
 	}
