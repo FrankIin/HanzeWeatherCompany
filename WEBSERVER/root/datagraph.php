a4611b10bfda4a41b39ac2ea8af15918fe4ff0a9
kevinj93 <keviny.19933@gmail.com>
2019-01-24 12:30:17 +0100

    graph positioning

---
+++ WEBSERVER/root/datagraph.php
@@ -0,0 +1,25 @@
+<html>
+
+<style>
+
+.graph_frame
+{
+	
+	position: absolute;
+	display: inline-block;
+	text-align: center;
+	width: 42%;
+	height: 45%;
+	right: 2%;
+	
+	
+	
+}
+
+</style>
+
+
+<div class = "graph_frame"> <div id = "myChart"> </div> </div>
+
+
+</html>
